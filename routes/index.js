d96997883d23e54326e8c1c377de5ec2d29366e7
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2021-05-28 13:15:42 +0200

    Add a macro

--- routes/index.js
+++ routes/index.js
@@ -11,14 +11,18 @@ export default function routes(app, addon) {
         // Rendering a template is easy; the render method takes two params: the name of the component or template file, and its props.
         // Handlebars and jsx are both supported, but please note that jsx changes require `npm run watch-jsx` in order to be picked up by the server.
         res.render(
-          'hello-world.hbs', // change this to 'hello-world.jsx' to use the Atlaskit & React version
-          {
-            title: 'Atlassian Connect'
-            //, issueId: req.query['issueId']
-            //, browserOnly: true // you can set this to disable server-side rendering for react views
-          }
+            'hello-world.hbs', // change this to 'hello-world.jsx' to use the Atlaskit & React version
+            {
+                title: 'Atlassian Connect'
+                //, issueId: req.query['issueId']
+                //, browserOnly: true // you can set this to disable server-side rendering for react views
+            }
         );
     });
 
     // Add additional route handlers here...
+    app.get('/timestamp', addon.authenticate(), async (req, res) => {
+        const year = '1988';
+        res.render('timestamp', {year})
+    });
 }
